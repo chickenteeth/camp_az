78a537de302b95406ede10163213caf272fcfdea
root <tstutzman@beardlysoft.com>
2019-04-25 22:32:09 +0000

    Initial deploy commit

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express	= require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+
+// show register form
+router.get("/register", function(req, res){
+   res.render("register", {page: 'register'}); 
+});
+
+//show login form
+router.get("/login", function(req, res){
+   res.render("login", {page: 'login'}); 
+});
+
+//Sign Up Logic
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            return res.render("register", {"error": err.message});
+        }
+        passport.authenticate("local")(req, res, function(){
+			req.flash("success", "Thanks for signing up, " + user.username);
+          	res.redirect("/campgrounds"); 
+        });
+    });
+});
+
+
+//Show Login Form
+router.get("/login", function(req, res){
+	res.render("login");
+});
+
+//Login Logic
+router.post("/login", passport.authenticate("local", {
+	successRedirect: "/campgrounds", 
+	failureRedirect:"/login"
+	}), function(req, res){
+});
+
+//Logout Route
+router.get("/logout", (req, res) => {
+	req.logout();
+	req.flash("success", "You have logged out");
+	res.redirect("/campgrounds");
+});
+
+module.exports = router;
