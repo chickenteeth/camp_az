0fad0286573636447e616ce440c26f0abd8eec98
Stutzman <tstutzman@phoenixchildrens.com>
2019-06-14 13:33:41 -0700

    change to trails

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-	flash		= require("connect-flash"),
-    passport    = require("passport"),
+require('dotenv').config();
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
     LocalStrategy = require("passport-local"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds"),
-	methodOverride = require("method-override");
-    
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    seedDB = require("./seeds"),
+    methodOverride = require("method-override");
+
 //requring routes
-var commentRoutes    = require("./routes/comments"),
+var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index");
+    indexRoutes = require("./routes/index");
 
 mongoose.connect(process.env.DATABASEURL, {
-	useNewUrlParser: true,
-	useCreateIndex: true
+    useNewUrlParser: true,
+    useCreateIndex: true
 }).then(() => {
-	console.log("Connected to DB");
+    console.log("Connected to DB");
 }).catch(err => {
-	console.log('error', err.message);
+    console.log('error', err.message);
 });
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -44,23 +45,23 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-  	res.locals.currentUser = req.user;
-  	res.locals.error = req.flash("error");
-  	res.locals.success = req.flash("success");
-   next();
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
 });
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("landing");
 });
 
 app.listen(process.env.PORT, process.env.IP);
 
 app.listen(3000, () => {
-	console.log("Server Listening");
+    console.log("Server Listening");
 });
